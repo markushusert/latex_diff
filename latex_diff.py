cbc8e7a65740302a179872bde7fc3a63d8ade4ef
markus <markus.husert@gmx.de>
2022-04-11 17:04:19 +0200

    create dirs for output

--- latex_diff.py
+++ latex_diff.py
@@ -18,14 +18,14 @@ def run_bash_cmd(cmd,dir="."):
         dir="."
     #print(f"cmd:{cmd},dir:{dir}")
     subprocess.run(cmd,cwd=dir,capture_output=False,shell=True,executable="/bin/bash")
-def generate_list_of_used_tex_files(main_tex_file):
+def generate_list_of_used_files(main_tex_file):
     used_files=[]
     used_filestructure=recreate_used_filestructure(main_tex_file)
 
     for dir,_,files in os.walk(used_filestructure):
         for file in files:
-            if file.endswith(".tex"):
-                used_files.append(os.path.join(dir,file))
+            used_files.append(os.path.join(dir,file))
+            
     return used_files,used_filestructure
 def recreate_used_filestructure(main_tex_file):
     """
@@ -36,8 +36,8 @@ def recreate_used_filestructure(main_tex_file):
     run_bash_cmd(f"mkjobtexmf --jobname {main_tex_without_ext}",main_tex_dir)
     return os.path.join(main_tex_dir,main_tex_without_ext+".mjt")
 def main():
-    changed_files,changed_basedir=generate_list_of_used_tex_files(args.changed)
-    orig_files,orig_basedir=generate_list_of_used_tex_files(args.orig)
+    changed_files,changed_basedir=generate_list_of_used_files(args.changed)
+    orig_files,orig_basedir=generate_list_of_used_files(args.orig)
 
     if args.output is not None:
         target_dir=args.output
@@ -54,11 +54,28 @@ def main():
         orig_file=os.path.join(orig_basedir,rel_path)
         changed_file=os.path.join(changed_basedir,rel_path)
         output_file=os.path.join(target_dir,rel_path)
-        for file in [orig_file,changed_file]:
-            if not os.path.exists(file):
-                open(file,"w").close()#create empty file if not existent
-        print(output_file)
-        run_bash_cmd(f"latexdiff {orig_file} {changed_file} >{output_file}")
+        outputdir=os.path.dirname(output_file)
+        os.makedirs(outputdir,exist_ok=True)
+
+        try:
+            os.remove(output_file)
+        except OSError:
+            pass
+
+        #texfiles processed via latexdiff
+        if (rel_path.endswith(".tex")):
+            for file in [orig_file,changed_file]:
+                if not os.path.exists(file):
+                    open(file,"w").close()#create empty file if not existent
+        
+            run_bash_cmd(f"latexdiff {orig_file} {changed_file} >{output_file}")
+        #other files (images are just linked)
+        else:
+            if os.path.exists(changed_file):
+                os.symlink(os.path.relpath(changed_file,outputdir),output_file)
+            elif os.path.exists(orig_file):
+                os.symlink(os.path.relpath(orig_file,outputdir),output_file)
+            
 
 
 if __name__=="__main__":
